eff9e595bac40bbccd614fa6e7f16f8212153909
Sameer Damir <sameer.damir@honeywell.com>
2018-08-08 16:34:40 +0200

    blabla

---
+++ solution1/console1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console1
+{
+    class Program
+    {
+        public static void ToPrint()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+           Program.ToPrint();
+            Program p1 = new Program();
+            System.Diagnostics.Debug.WriteLine(p1);
+            Console.ReadLine();
+        }
+    }
+}
